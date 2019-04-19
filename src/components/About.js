8e1a639aaeefafe9c11c702616c88031e31ab878
ivandgc <ivandgc@hotmail.com>
2019-04-18 20:26:07 -0500

    FEATURE: use HashRouter and responsiveness improvement

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../App.css'
 import clientsImage from '../res/our-clients.png'
 import teamImage from '../res/our-team.png'
-import { Segment, Header, Image, Divider } from 'semantic-ui-react'
+import { Segment, Header, Image, Divider, Grid } from 'semantic-ui-react'
 
 class About extends Component {
 
@@ -12,31 +12,43 @@ class About extends Component {
       <div>
         <Header textAlign='center' className='banner-name'>About Us</Header>
         <Divider></Divider>
-        <Segment basic className='home-segment'>
-          <Header textAlign='center' as='h1'>Our Clients</Header>
-          <p className='about-clients'>
-            Most of Reimpet International Corporation’s clients are currently
-             located on eastern Venezuela on the region near the Orinoco
-             Petroleum Belt (Largest Extra Heavy Crude Oil Reserve of the World)
-             and also in Colombia. They consist of exploration and drilling
-             companies, refineries, petrochemical plants, pipeline projects,
-             etc. With complete dedication to the energy industry, RIC will
-             provide expertise and any supplies needed for oil and gas projects.
-          </p>
-          <Image className='about-clients-picture' src={ clientsImage }/>
-        </Segment>
+        <Header textAlign='center' as='h1'>Our Clients</Header>
+        <Grid stackable container centered verticalAlign='middle'>
+          <Grid.Row>
+            <Grid.Column width={9}>
+              <p className='about-text'>
+                Most of Reimpet International Corporation’s clients are currently
+                located on eastern Venezuela on the region near the Orinoco
+                Petroleum Belt (Largest Extra Heavy Crude Oil Reserve of the World)
+                and also in Colombia. They consist of exploration and drilling
+                companies, refineries, petrochemical plants, pipeline projects,
+                etc. With complete dedication to the energy industry, RIC will
+                provide expertise and any supplies needed for oil and gas projects.
+              </p>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Image centered src={ clientsImage }/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
         <Divider></Divider>
-        <Segment basic textAlign='center' className='home-segment'>
-          <Header as='h1'>Our Team</Header>
-          <Image className='float-left team-picture' src={ teamImage }/>
-          <p className='float-right team-text'>
-            Reimpet International Corporation has a team with many years of
-            experience in the oil and gas industry. We are located on the largest
-            oilfield market of the world (Houston, Texas) and work hard to keep up
-            with the supply and demand of new and used equipment. Customer
-            satisfaction is our number one priority.
-          </p>
-        </Segment>
+        <Header textAlign='center' as='h1'>Our Team</Header>
+        <Grid stackable container centered verticalAlign='middle'>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Image centered src={ teamImage }/>
+            </Grid.Column>
+            <Grid.Column width={9}>
+              <p className='about-text'>
+                Reimpet International Corporation has a team with many years of
+                experience in the oil and gas industry. We are located on the largest
+                oilfield market of the world (Houston, Texas) and work hard to keep up
+                with the supply and demand of new and used equipment. Customer
+                satisfaction is our number one priority.
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     )
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Header, Image } from 'semantic-ui-react'
+import { Segment, Header, Image, Grid } from 'semantic-ui-react'
 import '../App.css'
 import landingImage from '../res/landing.png'
 
@@ -10,16 +10,24 @@ class Home extends Component {
     return(
       <div>
         <Header textAlign='center' className='banner-name'>Reimpet International Corporation</Header>
-        <Segment inverted color='green' className='home-segment'>
-          <Image circular className='float-left' src={ landingImage }/>
-          <p className='float-right'>
-            Reimpet International Corporation (RIC) is a diversified oilfield
-            supply company providing a complete range of equipment, parts, and
-            accessories for oil and gas related projects. In addition, RIC is
-            a well-resourced company, striving for excellence and total customer
-            satisfaction. We directly serve the end-users by offering our
-            exceptional supply and service.
-          </p>
+        <Segment inverted color='green'>
+          <Grid stackable container verticalAlign='middle' columns={2}>
+            <Grid.Row>
+              <Grid.Column>
+                <Image circular src={ landingImage }/>
+              </Grid.Column>
+              <Grid.Column>
+                <p className='intro-text'>
+                  Reimpet International Corporation (RIC) is a diversified oilfield
+                  supply company providing a complete range of equipment, parts, and
+                  accessories for oil and gas related projects. In addition, RIC is
+                  a well-resourced company, striving for excellence and total customer
+                  satisfaction. We directly serve the end-users by offering our
+                  exceptional supply and service.
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Segment>
       </div>
     )
